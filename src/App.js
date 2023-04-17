cfb00ea4dc09f85ee32206d9b882d863d191ca66
Satya Sharma <sattyasharma195@gmail.com>
2023-04-17 12:01:56 +0530

    added obbjects

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Navbar from './components/Navbar';
+import Products from './components/Products'
+function App() { 
+const product =[
+  {
+    price : 9999,
+    name : "iphone15",
+    quantity:0,
+  },
+  {
+    price : 9999,
+    name : "iphone14",
+    quantity:0,
+  }
+]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <Navbar/>
+  <Products/>
+  <Footer/>
+  </>
   );
 }
 
